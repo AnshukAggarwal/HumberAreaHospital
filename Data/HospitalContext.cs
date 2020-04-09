7b7f651122a4edad76e547ae3690a5eecaad86c2
Anshuk Aggarwal <aggar@LAPTOP-OSKI3RTS>
2020-04-09 02:57:23 -0400

    first commit

---
+++ Data/HospitalContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using HumberAreaHospitalProject.Models;
+
+namespace HumberAreaHospitalProject.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class HospitalContext: IdentityDbContext<ApplicationUser>
+    {
+        public HospitalContext() : base("name=HospitalContext")
+        {
+        }
+        public static HospitalContext Create()
+        {
+            return new HospitalContext();
+        }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Speciality> Specialities { get; set; }
+    }
+}
---
+++ Models/ViewModel/ViewDoctor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HumberAreaHospitalProject.Models.ViewModel
+{
+    public class ViewDoctor
+    {
+        public virtual Doctor doctor { get; set; }
+        public List<Doctor> similardoctors { get; set; }
+    }
+}
