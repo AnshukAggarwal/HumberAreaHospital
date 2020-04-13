c4c5d340a73740f19adc60e5a0adfa2115c4f09e
MarLMazo <marlmazo01@gmail.com>
2020-04-13 17:57:35 -0400

    Update Features View

--- Controllers/AuthorController.cs
+++ Controllers/AuthorController.cs
@@ -17,19 +17,72 @@ namespace HumberAreaHospitalProject.Controllers
         private HospitalContext db = new HospitalContext();
 
         //Accessible only for user that is login
-        [Authorize]
-        public ActionResult List()
+        //[Authorize]
+        public ActionResult List(string searchkey, int pagenum = 1)
         {
+            //Christine Bittle In-Class Example
             //Query to get All the Authors    
             string query = "Select * from Authors";
             //Debug.WriteLine(query);
+            //Debug.WriteLine(query);
             //Run the Sql command
-            List<Author> authors = db.Authors.SqlQuery(query).ToList();
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            //Searchkey is not empty
+            if (searchkey != "")
+            {
+                //Add to the query for Search
+                query = query + " where AuthorFname like @searchkey";
+                parameters.Add(new SqlParameter("@searchkey", "%" + searchkey + "%"));
+            }
+            //Execute the sql query
+            List<Author> authors = db.Authors.SqlQuery(query, parameters.ToArray()).ToList();
+            //Add Variable for how many per page
+            int perpage = 3;
+            //Debug.WriteLine("Authors Count is " + authors.Count());
+            //Get the total count values 
+            int volunteercount = authors.Count();
+            //Max page will be the Count Divided by per page. Rounding this to the upper limit so that
+            //rows which are not divisible to the perpage will still be shown
+            int maxpage = (int)Math.Ceiling((decimal)volunteercount / perpage);
+            //Maxpage is greater than 0, maxpage will be zero
+            if (maxpage < 0) maxpage = 0;
+            //Page number less than 1 will always be 1
+            if (pagenum < 1) pagenum = 1;
+            //If page number is greater than maxpage, pagenum will be equal to maxpag
+            if (pagenum > maxpage) pagenum = maxpage;
+            //Start value should be 0, but should be ascending depending on the perpage value
+            //Start with index 0, and then next will be 3, since perpage is 3. So the next page will start with index 3.
+            //The first page will show 0,1,2 index row and the next page will be 3,4,5 indexes and so on. 
+            int start = (int)(perpage * pagenum) - perpage;
+            Debug.WriteLine("start number is: " + start);
+            ViewData["pagenum"] = pagenum;
+            ViewData["pagesummary"] = "";
+            ViewData["maxpage"] = maxpage;
+            if (maxpage > 0)
+            {
+                ViewData["pagesummary"] = (pagenum) + " of " + (maxpage);
+                List<SqlParameter> newparams = new List<SqlParameter>();
+
+                if (searchkey != "")
+                {
+                    newparams.Add(new SqlParameter("@searchkey", "%" + searchkey + "%"));
+                    ViewData["searchkey"] = searchkey;
+                }
+                newparams.Add(new SqlParameter("@start", start));
+                newparams.Add(new SqlParameter("@perpage", perpage));
+                string pagedquery = query + " order by AuthorID offset @start rows fetch first @perpage rows only ";
+                //Debug.WriteLine(pagedquery);
+                //Debug.WriteLine("offset " + start);
+                //Debug.WriteLine("fetch first " + perpage);
+                //Re-write the execution of sql query with page listing
+                authors = db.Authors.SqlQuery(pagedquery, newparams.ToArray()).ToList();
+            }
+
             return View(authors);
         }
 
         //Accessible only for user that is login
-        [Authorize]
+        //[Authorize]
         //Get View for the specific Author
         public ActionResult View(int id)
         {
@@ -39,7 +92,7 @@ namespace HumberAreaHospitalProject.Controllers
         }
 
         //Accessible only for user that is login
-        [Authorize]
+        //[Authorize]
         public ActionResult Create()
         {
             return View();
@@ -72,7 +125,7 @@ namespace HumberAreaHospitalProject.Controllers
         }
 
         //Accessible only for user that is login
-        [Authorize]
+        //[Authorize]
         public ActionResult Update(int? id)
         {
             if (id == null)
@@ -122,7 +175,7 @@ namespace HumberAreaHospitalProject.Controllers
         }
 
         //Use to apply also on the other view
-        [Authorize]
+        //[Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -142,7 +195,7 @@ namespace HumberAreaHospitalProject.Controllers
             return View(selectedauthor);
         }
 
-        
+
         [HttpPost]
         public ActionResult Delete(int id)
         {
--- Controllers/VolunteerController.cs
+++ Controllers/VolunteerController.cs
@@ -17,19 +17,72 @@ namespace HumberAreaHospitalProject.Controllers
         private HospitalContext db = new HospitalContext();
 
         //Accessible only for user that is login
-        [Authorize]
-        public ActionResult List()
+        //[Authorize]
+        public ActionResult List(string searchkey, int pagenum = 1)
         {
+            //Christine Bittle In-Class Example
             //Query to get All the VOlunteers
             string query = "Select * from Volunteers";
             //Debug.WriteLine(query);
-            //Run the Sql command
-            List<Volunteer> volunteers = db.Volunteers.SqlQuery(query).ToList();
+            //Start creating a Class for sql parameter
+            List<SqlParameter> parameters = new List<SqlParameter>();
+            //Searchkey is not empty
+            if (searchkey != "")
+            {
+                //Add to the query for Search
+                query = query + " where VolunteerFname like @searchkey OR VolunteerLname like @searchkey";
+                parameters.Add(new SqlParameter("@searchkey", "%" + searchkey + "%"));
+            }
+            //Execute the sql query for volunteers
+            List<Volunteer> volunteers = db.Volunteers.SqlQuery(query, parameters.ToArray()).ToList();
+            //Add Variable for how many per page 
+            int perpage = 3;
+            //Debug.WriteLine("Volunteer Count is "+volunteers.Count());
+            //Get the total count values of volunteers
+            int volunteercount = volunteers.Count();
+            //Max page will be the Count Divided by per page. Rounding this to the upper limit so that
+            //rows which are not divisible to the perpage will still be shown
+            int maxpage = (int)Math.Ceiling((decimal)volunteercount / perpage);
+            //Maxpage is greater than 0, maxpage will be zero
+            if (maxpage < 0) maxpage = 0;
+            //Page number less than 1 will always be 1
+            if (pagenum < 1) pagenum = 1;
+            //If page number is greater than maxpage, pagenum will be equal to maxpage
+            if (pagenum > maxpage) pagenum = maxpage;
+            //Start value should be 0, but should be ascending depending on the perpage value
+            //Start with index 0, and then next will be 3, since perpage is 3. So the next page will start with index 3.
+            //The first page will show 0,1,2 index row and the next page will be 3,4,5 indexes and so on. 
+            int start = (int)(perpage * pagenum) - perpage;
+            //Debug.WriteLine("start number is: "+start);
+            ViewData["pagenum"] = pagenum;
+            ViewData["pagesummary"] = "";
+            ViewData["maxpage"] = maxpage;
+            if (maxpage > 0)
+            {
+                ViewData["pagesummary"] = (pagenum) + " of " + (maxpage);
+                List<SqlParameter> newparams = new List<SqlParameter>();
+
+                if (searchkey != "")
+                {
+                    newparams.Add(new SqlParameter("@searchkey", "%" + searchkey + "%"));
+                    ViewData["searchkey"] = searchkey;
+                }
+                newparams.Add(new SqlParameter("@start", start));
+                newparams.Add(new SqlParameter("@perpage", perpage));
+                string pagedquery = query + " order by VolunteerID offset @start rows fetch first @perpage rows only ";
+                //string pagedquery = query + " order by VolunteerID offset 1 rows fetch first 3 rows only ";
+                Debug.WriteLine(pagedquery);
+                //Debug.WriteLine("offset " + start);
+                //Debug.WriteLine("fetch first " + perpage);
+                //Re-write the execution of sql query with page listing
+                volunteers = db.Volunteers.SqlQuery(pagedquery, newparams.ToArray()).ToList();
+            }
+
             return View(volunteers);
         }
 
         //Accessible only for user that is login
-        [Authorize]
+        //[Authorize]
         //Get View for the specific volunteer
         public ActionResult View(int id)
         {
@@ -39,7 +92,7 @@ namespace HumberAreaHospitalProject.Controllers
         }
 
         //Use to apply also on the other view
-        [ActionName("Apply_today")]
+        //[ActionName("Apply_today")]
         //This function is needed for the User to Show as a different URL
         public ActionResult Create()
         {
@@ -50,7 +103,7 @@ namespace HumberAreaHospitalProject.Controllers
         [HttpPost]
         public ActionResult Create(string volunteerFname, string volunteerLname, string address, string email, string homePhone, string workPhone, string skills, ICollection<string> day, string time, string preference, string notes)
         {
-            
+
             String seperator = ",";
             string listdays = "";
             //All value on the days will be concatenated here
@@ -78,7 +131,7 @@ namespace HumberAreaHospitalProject.Controllers
 
                 db.Database.ExecuteSqlCommand(query, parameters);
                 // Debug.WriteLine("The new record being added in Volunteers");
-                
+
                 return RedirectToAction("List");
             }
             catch
@@ -89,7 +142,7 @@ namespace HumberAreaHospitalProject.Controllers
         }
 
         //Accessible only for user that is login
-        [Authorize]
+        //[Authorize]
         public ActionResult Update(int? id)
         {
             if (id == null)
@@ -97,7 +150,7 @@ namespace HumberAreaHospitalProject.Controllers
                 //If no id value has been presented, will return a BadRequest
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            
+
             string query = "Select * from Volunteers where VolunteerID=@id";
             //Debug.WriteLine(query);
             SqlParameter parameter = new SqlParameter("@id", id);
@@ -111,7 +164,7 @@ namespace HumberAreaHospitalProject.Controllers
             return View(selectedvolunteer);
         }
 
-        
+
         [HttpPost]
         public ActionResult Update(int id, string volunteerFname, string volunteerLname, string address, string email, string homePhone, string workPhone, string skills, ICollection<string> day, string time, string preference, string notes)
         {
@@ -151,12 +204,12 @@ namespace HumberAreaHospitalProject.Controllers
                 //Get Bad Request if Sql got error
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-           
+
 
         }
 
         //Use to apply also on the other view
-        [Authorize]
+        //[Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -195,6 +248,10 @@ namespace HumberAreaHospitalProject.Controllers
                 //Get Bad Request if Sql got error
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+        }
+        public ActionResult Apply_today()
+        {
+            return View();
         }
     }
 }
