192e50de236ab31006b72123c558225da822324b
Madhusudhanreddy1221 <madhusudhanreddy1221@live.com>
2020-04-14 04:15:59 -0400

    added erd

--- Models/ViewModels/ListSurvey.cs
+++ Models/ViewModels/ListSurvey.cs
@@ -1,15 +1,14 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace HumberAreaHospitalProject.Models.ViewModels
 {
-	public class ListSurvey
-	{
-        public virtual List<Question> questions { get; set; }
-        public virtual List<Survey> answers { get; set; }
-
-
-	}
+    public class ListSurvey
+    {
+        /*This class requires list of question and list of answers*/
+        public List<Question> questions { get; set; }
+        public List<Survey> surveys { get; set; }
+    }
 }
